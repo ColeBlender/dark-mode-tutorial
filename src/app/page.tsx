0a0f3c0876f18ba565521bf31d80b91369a66309
Cole Blender <coleblender@gmail.com>
2024-09-06 12:09:40 -0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { ModeToggle } from "@/components/ModeToggle";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ModeToggle />
+
+      <div className="bg-primary">
+        <p className="text-background">What up thoooo</p>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
